e0021b1c9e696b8b85293ae0e9cb14ebb9e498ff
Banula Lakwindu <64958389+banulalakwindu@users.noreply.github.com>
2023-05-10 23:13:02 +0530

    Login page created

--- devhive2023_front/src/pages/login.jsx
+++ devhive2023_front/src/pages/login.jsx
@@ -2,7 +2,26 @@ import React from 'react'
 
 const Login = () => {
     return (
-        <div>Login</div>
+        <div className='index_page vh-100 has-bg-img overflow-hidden d-flex'>
+            <img className='bg-img' src="/home.jpg" />
+
+            <div className="login-inner m-auto rounded d-flex flex-column text-white text-center">
+                <div>
+                    <img src="/Logo-Color.png" />
+                    <h4 className='mt-3 mb-1'>Student Management System</h4>
+                    <p>Faculty of Engineering - University of Jaffna</p>
+                </div>
+
+                <h1 className="mt-2">Login</h1>
+                <form className="mt-2">
+                    <div className="form-group px-4 py-4">
+                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+                        <input type="password" className="form-control mt-4" id="exampleInputPassword1" placeholder="Password" />
+                        <input type="submit" className="btn btn-primary mt-4" value="Login" />
+                    </div>
+                </form>
+            </div>
+        </div>
     )
 }
 
