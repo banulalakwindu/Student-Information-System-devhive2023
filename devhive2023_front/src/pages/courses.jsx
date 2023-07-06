df19e361cf18420f926417779cfecc52ab35dff4
Banula Lakwindu <64958389+banulalakwindu@users.noreply.github.com>
2023-07-06 10:37:55 +0530

    Courses page updated

--- devhive2023_front/src/pages/courses.jsx
+++ devhive2023_front/src/pages/courses.jsx
@@ -12,10 +12,10 @@ const courses = () => {
                 <div className="semester-inner-div d-flex flex-column mt-4">
                     <div className="zoom my-2 d-flex justify-content-center btn-light-cus rounded" style={{ height: "180px" }}><h5 className='m-auto text-uppercase'>General Program of the Degree Program</h5></div>
                     <div className="d-flex my-2 justify-content-between">
-                        <div className="zoom d-flex justify-content-center btn-light-cus rounded" style={{ width: "300px", height: "180px" }} ><h5 className='my-auto mx-2 py-2 text-uppercase'>Specialization in Civil and Environmental Engineering</h5></div>
-                        <div className="zoom d-flex justify-content-center btn-light-cus rounded" style={{ width: "300px", height: "180px" }} ><h5 className='my-auto mx-2 text-uppercase'>Specialization in Computer Engineering</h5></div>
-                        <div className="zoom d-flex justify-content-center btn-light-cus rounded" style={{ width: "300px", height: "180px" }} ><h5 className='my-auto mx-2 text-uppercase'>Specialization in Electrical and Electronic Engineering</h5></div>
-                        <div className="zoom d-flex justify-content-center btn-light-cus rounded" style={{ width: "300px", height: "180px" }} ><h5 className='my-auto mx-2 text-uppercase'>Specialization in Mechanical and Process Engineering</h5></div>
+                        <div className="zoom me-2 d-flex justify-content-center btn-light-cus rounded" style={{ width: "300px", height: "180px" }} ><h5 className='my-auto mx-2 py-2 text-uppercase'>Specialization in Civil and Environmental Engineering</h5></div>
+                        <div className="zoom mx-2 d-flex justify-content-center btn-light-cus rounded" style={{ width: "300px", height: "180px" }} ><h5 className='my-auto mx-2 text-uppercase'>Specialization in Computer Engineering</h5></div>
+                        <div className="zoom mx-2 d-flex justify-content-center btn-light-cus rounded" style={{ width: "300px", height: "180px" }} ><h5 className='my-auto mx-2 text-uppercase'>Specialization in Electrical and Electronic Engineering</h5></div>
+                        <div className="zoom ms-2 d-flex justify-content-center btn-light-cus rounded" style={{ width: "300px", height: "180px" }} ><h5 className='my-auto mx-2 text-uppercase'>Specialization in Mechanical and Process Engineering</h5></div>
                     </div>
                     <div className="zoom my-2 d-flex justify-content-center btn-light-cus rounded" style={{ height: "180px" }}><h5 className='m-auto text-uppercase'>Complementary Studies</h5></div>
                 </div>
