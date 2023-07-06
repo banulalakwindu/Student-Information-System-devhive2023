29c3e56379666c9150e2265435f0b8d844928a51
Banula Lakwindu <64958389+banulalakwindu@users.noreply.github.com>
2023-07-06 10:20:47 +0530

    Footer Button Updated

--- devhive2023_front/src/components/Footer.jsx
+++ devhive2023_front/src/components/Footer.jsx
@@ -7,10 +7,10 @@ const Footer = () => {
     return (
         <div>
             <footer className='fixed-bottom bg-green d-flex shadow border-top text-white rounded-top justify-content-between px-5' style={{ height: "75px" }}>
-                <div className='btn btn-light-cus my-auto d-flex'><BsGlobe className='me-2 my-auto' />eng.jfn.ac.lk</div>
-                <div className='btn btn-light-cus my-auto d-flex'><FaEnvelope className='me-2 my-auto' />admin@eng.jfn.ac.lk</div>
-                <div className='btn btn-light-cus my-auto d-flex'><GiRotaryPhone className='me-2 my-auto' />+94-021-2062161</div>
-                <div className='btn btn-light-cus my-auto d-flex'><FaFacebook className='me-2 my-auto' />FOE | UOJ</div>
+                <div className='btn btn-green border my-auto d-flex'><BsGlobe className='me-2 my-auto' />eng.jfn.ac.lk</div>
+                <div className='btn btn-green border my-auto d-flex'><FaEnvelope className='me-2 my-auto' />admin@eng.jfn.ac.lk</div>
+                <div className='btn btn-green border my-auto d-flex'><GiRotaryPhone className='me-2 my-auto' />+94-021-2062161</div>
+                <div className='btn btn-green border my-auto d-flex'><FaFacebook className='me-2 my-auto' />FOE | UOJ</div>
             </footer>
         </div>
     )
