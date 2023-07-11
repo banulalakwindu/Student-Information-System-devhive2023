650c82203c49a9a021c57a3f846d12f0a11e7fbe
Banula Lakwindu <64958389+banulalakwindu@users.noreply.github.com>
2023-07-11 10:22:29 +0530

    Mockups updated

--- devhive2023_front/src/pages/profile.jsx
+++ devhive2023_front/src/pages/profile.jsx
@@ -4,7 +4,9 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
 import Footer from '../components/Footer'
 
+
 const Profile = () => {
+
     return (
         <div>
             <Navbar />
