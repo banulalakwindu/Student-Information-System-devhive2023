d3b710e1d331b5a9c013b05b765118e152f9bdf9
WIJETHUNGA-174 <2019e174@eng.jfn.ac.lk>
2023-07-17 01:58:26 +0530

    model index updated

--- devhive2023_back/src/models/index.js
+++ devhive2023_back/src/models/index.js
@@ -1,37 +1,54 @@
-'use strict';
-
-const fs = require('fs');
-const path = require('path');
 const Sequelize = require('sequelize');
 const process = require('process');
-const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+
+const App_DB = process.env.APP_DB || 'devhive2023';
+const APP_USER = process.env.APP_USER || 'root';
+const APP_PASS = process.env.APP_PASS || null;
+
+const mysql = require('mysql2');
+
+const academicstaff = require('./academicstaff');
+const advisorhistory = require('./advisorhistory');
+const course = require('./course');
+const coursehistoryoffered = require('./coursehistoryoffered');
+const courseregistration = require('./courseregistration');
+const department = require('./department');
+const departmentcourse = require('./departmentcourse');
+const medicalsubmission = require('./medicalsubmission');
+const prerequestcoursedetails = require('./prerequestcoursedetails');
+const result = require('./result');
+const semesterdetails = require('./semesterdetails');
+const studentacademic = require('./studentacademic');
+const studentregistration = require('./studentregistration');
+const studentunivasitydetails = require('./studentunivasitydetails');
+
 const db = {};
+const sequelize = new Sequelize(App_DB, APP_USER, APP_PASS, {
+  host: 'localhost',
+  dialect: 'mysql',
+  dialectModule: mysql2,
+});
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+db.academicstaff = academicstaff(sequelize, Sequelize.DataTypes);
+db.advisorhistory = advisorhistory(sequelize, Sequelize.DataTypes);
+db.course = course(sequelize, Sequelize.DataTypes); 
+db.coursehistoryoffered = coursehistoryoffered(sequelize, Sequelize.DataTypes);
+db.courseregistration = courseregistration(sequelize, Sequelize.DataTypes);
+db.department = department(sequelize, Sequelize.DataTypes);
+db.departmentcourse = departmentcourse(sequelize, Sequelize.DataTypes);
+db.medicalsubmission = medicalsubmission(sequelize, Sequelize.DataTypes);
+db.prerequestcoursedetails = prerequestcoursedetails(sequelize, Sequelize.DataTypes);
+db.result = result(sequelize, Sequelize.DataTypes);
+db.semesterdetails = semesterdetails(sequelize, Sequelize.DataTypes);
+db.studentacademic = studentacademic(sequelize, Sequelize.DataTypes);
+db.studentregistration = studentregistration(sequelize, Sequelize.DataTypes);
+db.studentunivasitydetails = studentunivasitydetails(sequelize, Sequelize.DataTypes);
 
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return ( 
-      file.indexOf('.') !== 0 &&
-      file !== basename &&
-      file.slice(-3) === '.js' &&
-      file.indexOf('.test.js') === -1
-    );
-  })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-    db[model.name] = model;
-  });
+const { academicstaff, advisorhistory, course, coursehistoryoffered, courseregistration, department, departmentcourse, medicalsubmission, prerequestcoursedetails, result, semesterdetails, studentacademic, studentregistration, studentunivasitydetails } = db;
+//associations
+academicstaff.hasMany(advisorhistory, { as: 'advisorhistory', foreignKey: 'advisorid'});
 
-Object.keys(db).forEach(modelName => {
+Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
@@ -39,5 +56,4 @@ Object.keys(db).forEach(modelName => {
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
-
 module.exports = db;
