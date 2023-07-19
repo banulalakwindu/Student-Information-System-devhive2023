1a17f85fd1f4cad284156f94b0b23d9a363b2b2c
2019e166@eng.jfn.ac.lk <2019e166@eng.jfn.ac.lk>
2023-07-19 09:30:23 +0530

    insertRegisterCourse & saveCourseRegistration
    After registration courses add to database and view the database data after submitting save button.

--- devhive2023_back/src/controllers/courseRegister.js
+++ devhive2023_back/src/controllers/courseRegister.js
@@ -1,17 +1,27 @@
-const { Department } = require('../models');
+const { courses, course_registration, academic_staff } = require('../models');
 
 module.exports = {
   async getResults(req, res) {
     try {
-      const { code } = req.params;
-      const results = await Department.findAll({
-        attributes: ['Department_ID', 'Department_Name', 'createdAt', 'updatedAt'],
+      const results = await courses.findAll({
+        attributes: ['course_code', 'course_name', 'credit'],
+        include: [
+          {
+            model: course_registration,
+            attributes: [],
+            where: { OpenClose: '1' },
+          },
+          {
+            model: academic_staff,
+            attributes: ['Full_Name'],
+          },
+        ],
+        where: { offered_semester: '5' },
       });
+
       res.status(200).json(results);
     } catch (error) {
       res.status(500).json(error);
     }
   }
 };
-
-
