e611fcc1a5a2423246b943b8a971b798a5d2c065
2019e166@eng.jfn.ac.lk <2019e166@eng.jfn.ac.lk>
2023-07-19 07:40:24 +0530

    getPrerequest.js
    getPrerequest was created.

---
+++ devhive2023_back/src/controllers/getPrerequest.js
@@ -0,0 +1,46 @@
+const { Op } = require('sequelize');
+const { Course, prerequisiteCourseDetails, Result } = require('../models');
+
+module.exports = {
+  async getCources(req, res) {
+    try {
+      const { code } = req.params; // Extract the 'code' parameter from req.params
+      const results = await Course.findAll({
+        attributes: ['Course_Code', 'Course_Name', 'Credit'],
+        where: { Course_Code: code },
+        include: [
+          {
+            model: Prerequisite_course_details,
+            as: 'prerequisiteCourses',
+            attributes: ['Prerequisite_Course_code'],
+          },
+          {
+            model: Result,
+            as: 'courseResults',
+            attributes: ['Result'],
+            where: { Reg_Number: '2019/E/099' },
+          },
+        ],
+      });
+
+      const coursesData = results.map((course) => {
+        const prerequisiteCourseDetails =
+          course.prerequisiteCourses.map((prerequisite) => prerequisite.Prerequisite_Course_code);
+
+        const result = course.courseResults[0] ? course.courseResults[0].Result : null;
+
+        return {
+          Course_Code: course.Course_Code,
+          Course_Name: course.Course_Name,
+          Credit: course.Credit,
+          Prerequisite_Course_code: prerequisiteCourseDetails,
+          Result: result,
+        };
+      });
+
+      res.status(200).json(coursesData);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  },
+};
