c4f66c6781cf614c2fb3049158e25d873920926b
Banuka Dananjaya <61711700+bannukadananjaya@users.noreply.github.com>
2023-07-20 08:02:38 +0530

    add tests in login page

--- devhive2023_front/src/__tests__/login.test.jsx
+++ devhive2023_front/src/__tests__/login.test.jsx
@@ -4,8 +4,35 @@ import '@testing-library/jest-dom/extend-expect'
 
 import Login from '../pages/login'
 
-test("render login page", () =>{
-    render(<Login/>)
-    const title = screen.getByText('Faculty of Engineering - University of Jaffna')
-    expect(title).toBeInTheDocument()
+describe("Testing the login page", () => {
+
+    test("render the heading", () => {
+        render(<Login />)
+        const heading = screen.getByText('Student Management System')
+        expect(heading).toBeInTheDocument()
+    });
+
+    test("render the sub heading", () =>{
+        render(<Login/>)
+        const subheading = screen.getByText('Faculty of Engineering - University of Jaffna')
+        expect(subheading).toBeInTheDocument()
+    });
+
+    //test for input fields
+    test("render input fields", () => {
+        render(<Login />)
+
+        const email = screen.getByRole("textbox")
+        expect(email).toBeInTheDocument()
+
+        const password = screen.getByRole("textbox")
+        expect(password).toBeInTheDocument()
+
+        const other = screen.getByRole("textbox")
+        expect(other).toBeInTheDocument()
+    })
+    
+    //test for the submit button
+
+
 });
