c2745cde332732b34cf47682a2bd14f22f6efdac
Mihiri <110478627+MIHIRI099@users.noreply.github.com>
2023-07-24 01:57:18 +0530

    frontendconnect_profile

--- devhive2023_front/src/pages/profile.jsx
+++ devhive2023_front/src/pages/profile.jsx
@@ -51,11 +51,11 @@ const Profile = () => {
                                     <p className='bg-white rounded p-2 fw-semibold' style={{ width: "400px" }}>Degree :- <span className='fw-normal fst-italic'>Bsc. Engineering</span></p>
                                 </div>
                                 <p className='bg-white rounded p-2 fw-semibold w-100'>Name with Initials :- <span className='fw-normal fst-italic'>{student.user.studentregistration.Name_With_Initial}</span></p>
-                                <p className='bg-white rounded p-2 fw-semibold w-100'>Full Name :- <span className='fw-normal fst-italic'>HIRILIYEDDA VIDANALAGE BANULA LAKWINDU CHANDRASIRI</span></p>
+                                <p className='bg-white rounded p-2 fw-semibold w-100'>Full Name :- <span className='fw-normal fst-italic'>{student.user.studentregistration.Full_Name}</span></p>
                                 <div className='d-flex justify-content-between'>
-                                    <p className='bg-white rounded p-2 fw-semibold' style={{ width: "200px" }}>Gender :- <span className='fw-normal fst-italic'>Male</span></p>
-                                    <p className='bg-white rounded p-2 fw-semibold' style={{ width: "200px" }}>Status :- <span className='fw-normal fst-italic'>Mr</span></p>
-                                    <p className='bg-white rounded p-2 fw-semibold' style={{ width: "400px" }}>Department :- <span className='fw-normal fst-italic'>Computer Engineering</span></p>
+                                    <p className='bg-white rounded p-2 fw-semibold' style={{ width: "200px" }}>Gender :- <span className='fw-normal fst-italic'>{student.user.studentregistration.Gender}</span></p>
+                                    <p className='bg-white rounded p-2 fw-semibold' style={{ width: "200px" }}>Status :- <span className='fw-normal fst-italic'>{student.user.studentregistration.Status}</span></p>
+                                    <p className='bg-white rounded p-2 fw-semibold' style={{ width: "400px" }}>Department :- <span className='fw-normal fst-italic'>{student.user.Department_ID}</span></p>
                                 </div>
                             </div>
                         </div>
@@ -63,21 +63,21 @@ const Profile = () => {
                             <div style={{ width: "570px" }}>
                                 <h3 className='text-center rounded py-2 bg-green text-white'>Emergency Contact Deatils</h3>
                                 <div className='p-3'>
-                                    <p className='bg-white rounded p-2 fw-semibold'>Emergency Contact Name :-<span className='ms-2 fw-normal fst-italic'>H.V.Chandrasiri</span></p>
-                                    <p className='bg-white rounded p-2 fw-semibold'>Relationship to the Student :-<span className='ms-2 fw-normal fst-italic'>Father</span></p>
-                                    <p className='bg-white rounded p-2 fw-semibold'>Emergency Phone Number :-<span className='ms-2 fw-normal fst-italic'>0716101341</span></p>
-                                    <p className='bg-white rounded p-2 fw-semibold'>Emergency Address :-<span className='ms-2 fw-normal fst-italic'>104/2/B, Pitakumbura Rd, Gonakumbura, Ratnapura</span></p>
-                                    <p className='bg-white rounded p-2 fw-semibold'>*Emergency Email :-<span className='ms-2 fw-normal fst-italic'>chandrasirihv@gmail.com</span></p>
+                                    <p className='bg-white rounded p-2 fw-semibold'>Emergency Contact Name :-<span className='ms-2 fw-normal fst-italic'>{student.user.studentregistration.Emergency_Contact_Name}</span></p>
+                                    <p className='bg-white rounded p-2 fw-semibold'>Relationship to the Student :-<span className='ms-2 fw-normal fst-italic'>{student.user.studentregistration.Emergency_Relationship_To_The_Student}</span></p>
+                                    <p className='bg-white rounded p-2 fw-semibold'>Emergency Phone Number :-<span className='ms-2 fw-normal fst-italic'>{student.user.studentregistration.Emergency_Phone_Number}</span></p>
+                                    <p className='bg-white rounded p-2 fw-semibold'>Emergency Address :-<span className='ms-2 fw-normal fst-italic'>{student.user.studentregistration.Emergency_Address}</span></p>
+                                    <p className='bg-white rounded p-2 fw-semibold'>*Emergency Email :-<span className='ms-2 fw-normal fst-italic'>{student.user.studentregistration.Personal_Email}</span></p>
                                 </div>
                             </div>
                             <div style={{ width: "570px" }}>
                                 <h3 className='text-center rounded py-2 bg-green text-white'>Private Details</h3>
                                 <div className='p-3'>
-                                    <p className='bg-white rounded p-2 fw-semibold'>Email Address (Personal) :-<span className='ms-2 fw-normal fst-italic'>banulalakwindu10@gmail.com</span></p>
-                                    <p className='bg-white rounded p-2 fw-semibold'>Mobile Number :-<span className='ms-2 fw-normal fst-italic'>0702686990</span></p>
-                                    <p className='bg-white rounded p-2 fw-semibold'>Home Number :-<span className='ms-2 fw-normal fst-italic'>0452222966</span></p>
-                                    <p className='bg-white rounded p-2 fw-semibold'>Permanent Address :-<span className='ms-2 fw-normal fst-italic'>104/2/B, Pitakumbura Rd, Gonakumbura, Ratnapura</span></p>
-                                    <p className='bg-white rounded p-2 fw-semibold'>Temporary Address :-<span className='ms-2 fw-normal fst-italic'>104/2/B, Pitakumbura Rd, Gonakumbura, Ratnapura</span></p>
+                                    <p className='bg-white rounded p-2 fw-semibold'>Email Address (Personal) :-<span className='ms-2 fw-normal fst-italic'>{student.user.studentregistration.Personal_Email}</span></p>
+                                    <p className='bg-white rounded p-2 fw-semibold'>Mobile Number :-<span className='ms-2 fw-normal fst-italic'>{student.user.studentregistration.Phone_Number}</span></p>
+                                    <p className='bg-white rounded p-2 fw-semibold'>Home Number :-<span className='ms-2 fw-normal fst-italic'>{student.user.studentregistration.Home_Number}</span></p>
+                                    <p className='bg-white rounded p-2 fw-semibold'>Permanent Address :-<span className='ms-2 fw-normal fst-italic'>{student.user.studentregistration.Permenant_Address}</span></p>
+                                    <p className='bg-white rounded p-2 fw-semibold'>Temporary Address :-<span className='ms-2 fw-normal fst-italic'>{student.user.studentregistration.Temporary_Address}</span></p>
                                 </div>
                             </div>
                         </div>
@@ -85,21 +85,21 @@ const Profile = () => {
                             <div style={{ width: "570px" }}>
                                 <h3 className='text-center rounded py-2 bg-green text-white'>University Selection Details</h3>
                                 <div className='p-3'>
-                                    <p className='bg-white rounded p-2 fw-semibold'>Registration Date :-<span className='ms-2 fw-normal fst-italic'>H.V.Chandrasiri</span></p>
-                                    <p className='bg-white rounded p-2 fw-semibold'>A/L Index Number :-<span className='ms-2 fw-normal fst-italic'>Father</span></p>
-                                    <p className='bg-white rounded p-2 fw-semibold'>Applied Year :-<span className='ms-2 fw-normal fst-italic'>0716101341</span></p>
-                                    <p className='bg-white rounded p-2 fw-semibold'>Medium :-<span className='ms-2 fw-normal fst-italic'>104/2/B, Pitakumbura Rd, Gonakumbura, Ratnapura</span></p>
-                                    <p className='bg-white rounded p-2 fw-semibold'>Z-score :-<span className='ms-2 fw-normal fst-italic'>chandrasirihv@gmail.com</span></p>
+                                    <p className='bg-white rounded p-2 fw-semibold'>Registration Date :-<span className='ms-2 fw-normal fst-italic'>{student.user.studentregistration.Registration_Date}</span></p>
+                                    <p className='bg-white rounded p-2 fw-semibold'>A/L Index Number :-<span className='ms-2 fw-normal fst-italic'>{student.user.studentregistration.AL_Index_Number}</span></p>
+                                    <p className='bg-white rounded p-2 fw-semibold'>Applied Year :-<span className='ms-2 fw-normal fst-italic'>{student.user.studentregistration.Applied_Year}</span></p>
+                                    <p className='bg-white rounded p-2 fw-semibold'>Medium :-<span className='ms-2 fw-normal fst-italic'>{student.user.studentregistration.Medium}</span></p>
+                                    <p className='bg-white rounded p-2 fw-semibold'>Z-score :-<span className='ms-2 fw-normal fst-italic'>{student.user.studentregistration.Z_Score}</span></p>
                                 </div>
                             </div>
                             <div style={{ width: "570px" }}>
                                 <h3 className='text-center rounded py-2 bg-green text-white'>Statistical Details</h3>
                                 <div className='p-3'>
-                                    <p className='bg-white rounded p-2 fw-semibold'>Race :-<span className='ms-2 fw-normal fst-italic'>Sinhala</span></p>
-                                    <p className='bg-white rounded p-2 fw-semibold'>Religion :-<span className='ms-2 fw-normal fst-italic'>Buddhism</span></p>
-                                    <p className='bg-white rounded p-2 fw-semibold'>Foreign :-<span className='ms-2 fw-normal fst-italic'>Sri Lanka</span></p>
-                                    <p className='bg-white rounded p-2 fw-semibold'>Nationality :-<span className='ms-2 fw-normal fst-italic'>Sri Lanka</span></p>
-                                    <p className='bg-white rounded p-2 fw-semibold'>Citizenship :-<span className='ms-2 fw-normal fst-italic'>Yes</span></p>
+                                    <p className='bg-white rounded p-2 fw-semibold'>Race :-<span className='ms-2 fw-normal fst-italic'>{student.user.studentregistration.Race}</span></p>
+                                    <p className='bg-white rounded p-2 fw-semibold'>Religion :-<span className='ms-2 fw-normal fst-italic'>{student.user.studentregistration.Religion}</span></p>
+                                    <p className='bg-white rounded p-2 fw-semibold'>Foreign :-<span className='ms-2 fw-normal fst-italic'>{student.user.studentregistration.Country}</span></p>
+                                    <p className='bg-white rounded p-2 fw-semibold'>Nationality :-<span className='ms-2 fw-normal fst-italic'>{student.user.studentregistration.Nationality}</span></p>
+                                    <p className='bg-white rounded p-2 fw-semibold'>Citizenship :-<span className='ms-2 fw-normal fst-italic'>{student.user.studentregistration.Citizenship}</span></p>
                                 </div>
                             </div>
                         </div>
