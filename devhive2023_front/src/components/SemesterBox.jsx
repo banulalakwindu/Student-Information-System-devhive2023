3104b53633077390eb3d077b361c83af7875f7b8
Banula Lakwindu <64958389+banulalakwindu@users.noreply.github.com>
2023-07-30 15:09:59 +0530

    Semster boxes updated

--- devhive2023_front/src/components/SemesterBox.jsx
+++ devhive2023_front/src/components/SemesterBox.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
+import { BsFillCheckCircleFill, BsPencilFill, BsClockFill } from "react-icons/bs";
 
-const SemesterBox = ({ text, offer, link }) => {
+
+const SemesterBox = ({ text, offer, link, completed }) => {
     return (
-        <a href={(offer == 1) ? `/semesters/registration?sem=${link}` : "#"} style={{ width: "200px", height: "200px" }} className={'d-flex mx-2 rounded ' + ((offer == 1) ? 'btn-light-cus zoom' : 'bg-disabled') } >
+        <a href={(offer == 1) ? `/semesters/registration?sem=${link}` : "#"} style={{ width: "200px", height: "200px" }} className={'d-flex flex-column mx-2 rounded zoom ' + ((offer == 1) ? 'btn-light-cus' : ((completed == 1) ? 'btn-light-cus completed-sem' : 'bg-disabled'))} >
             <h5 className='m-auto text-uppercase'>{text}</h5>
-        </a>
+            {(completed == 1) ? (
+                <div className='d-flex flex-column'>
+                    <BsFillCheckCircleFill className='mx-auto mb-auto' />
+                    <small className='m-auto mb-2'>Click to View</small>
+                </div>
+            ) : (
+                (offer == 1) ? (
+                    <div className='d-flex flex-column'>
+                        <BsPencilFill className='mx-auto mb-auto' />
+                        <small className='m-auto mb-2'>Click to Register</small>
+                    </div>
+                ) : (
+                    <div className='d-flex flex-column'>
+                        <BsClockFill className='mx-auto mb-auto' />
+                        <small className='m-auto mb-2'>Wait until Semester Open</small>
+                    </div>
+                ))
+            }
+        </a >
     )
 }
 
